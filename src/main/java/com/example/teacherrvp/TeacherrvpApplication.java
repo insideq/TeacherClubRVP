08e3eb7fae3cfce63bfa91ac5321d04cee106e25
insideq <crazyandroed@gmail.com>
2026-09-11 15:27:30 +0400

    first commit

---
+++ src/main/java/com/example/teacherrvp/TeacherrvpApplication.java
@@ -0,0 +1,13 @@
+package com.example.teacherrvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherrvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherrvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teacherrvp/TeacherrvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teacherrvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherrvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
